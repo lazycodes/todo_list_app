f2cb199152bec74e07fdd539f2930f50d81c497c
paulgnz <work@paulgrey.co.nz>
2015-07-09 23:17:30 +1200

    Add length and presence validation to todolist title

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -30,10 +30,32 @@ describe "Creating todo lists", :js, :driver => :webkit do
 
 		expect(page).to have_content("Title can't be blank")
 		expect(TodoList.count).to eq(0)
-		save_and_open_page
-
+		#save_and_open_page
+		
+		visit "/todo_lists"
 		expect(page).to_not have_content("Capybara test description")
 
 	end
+
+	it "displays an error when the todo list has a title less than three characters", :js, :driver => :webkit do
+		expect(TodoList.count).to eq(0)
+
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New Todo List")
+
+		fill_in "Title", with: "Hi"
+		fill_in "Description", with: "Capybara test description"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("Title is too short")
+		expect(TodoList.count).to eq(0)
+		#save_and_open_page
+		
+		visit "/todo_lists"
+		expect(page).to_not have_content("Capybara test description")
+
+	end	
 
 end
