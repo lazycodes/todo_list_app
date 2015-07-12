77793181c429ad3f59bbe8a6a9981dce86844749
paulgnz <work@paulgrey.co.nz>
2015-07-12 21:23:13 +1200

    Add todo item model and associated specs

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ ruby '2.2.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
 # Use postgresql as the database for Active Record
-gem 'pg'
+gem "pg", "~> 0.18.2"
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -55,6 +55,7 @@ group :test do
   gem 'launchy'
   gem 'database_cleaner'
   gem 'rspec-activemodel-mocks'
+  gem 'shoulda-matchers'
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708115738) do
+ActiveRecord::Schema.define(version: 20150712050905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20150708115738) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "todo_items", force: :cascade do |t|
+    t.integer  "todo_list_id"
+    t.string   "content"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id", using: :btree
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
@@ -51,4 +60,5 @@ ActiveRecord::Schema.define(version: 20150708115738) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "todo_items", "todo_lists"
 end
