03d43ea959a66276e7bfe997a0082821b3c8b70d
paulgnz <work@paulgrey.co.nz>
2015-07-14 19:41:00 +1200

    Add Rails Dom ID Helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,7 +23,7 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -31,6 +31,8 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
 
+  config.include TodoListHelpers, type: :feature
+  config.include RailsDomIdHelper, type: :feature
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
